d00de1f30434f9887b46253627b831b13a76cd53
Constantin Wang <850901938@qq.com>
2017-03-05 13:39:07 +0800

    Fifth Day

---
+++ 实例17&18：在画布上绘制商标&使用RGraph实现图表统计/使用RGraph实现图表统计/rgraph.js
@@ -0,0 +1,23 @@
+var canvasGraph = function(){
+    var title = $('#graph_data h1').text();
+    var labels = $("#graph_data>ul>li>p[data-name]").map(function(){
+        return this.getAttribute("data-name");
+    });
+    var percents = $("#graph_data>ul>li>p[data-percent]").map(function(){
+        return parseInt(this.getAttribute("data-percent"));
+    });
+    var bar = new RGraph.Bar('browsers',percents);
+    bar.Set('chart.gutter',50);
+    bar.Set('chart.colors',['red']);
+    bar.Set('chart.title',title);
+    bar.Set('chart.labels',labels);
+    bar.Draw();
+    $('#graph_data').hide();
+}
+
+$(document).ready(function(){
+    var canvas = document.getElementById('browsers');
+    if(canvas.getContext){
+        canvasGraph();
+    }
+});
